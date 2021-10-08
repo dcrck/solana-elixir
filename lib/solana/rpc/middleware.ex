273c076f11cd94dba290a258c972076e49ab3dbd
dcrck <derek@meer.email>
2021-10-08 12:03:12 -0700

    adjust Solana.RPC.Middleware to decode responses
    oftentimes RPC API responses have base58- or base64-encoded fields. We
    can decode them since we know which request corresponds to each
    response, so we know the structure of each response. This means we won't
    need to decode responses when calling the top-level module functions, so
    we can feed them directly into other API request geneators (which assume
    the input arguments aren't encoded).
    
    For now, this only decodes responses for:
    
    - requestAirdrop
    - sendTransaction
    - getRecentBlockhash

--- lib/solana/rpc/middleware.ex
+++ lib/solana/rpc/middleware.ex
@@ -3,29 +3,51 @@ defmodule Solana.RPC.Middleware do
 
   @success 200..299
 
-  def call(env, next, _) do
+  def call(env = %{body: request}, next, _) do
     env
     |> Tesla.run(next)
-    |> handle_response()
+    |> handle_response(request)
   end
 
-  defp handle_response({:ok, response = %{status: status}})
+  defp handle_response({:ok, response = %{status: status}}, request)
        when status in @success do
-    {:ok, response_content(response)}
+    response_content(response, request)
   end
 
-  defp handle_response({:ok, %{status: status}}), do: {:error, status}
-  defp handle_response(other), do: other
+  defp handle_response({:ok, %{status: status}}, _), do: {:error, status}
+  defp handle_response(other, _), do: other
 
-  defp response_content(%{body: body}) when is_list(body) do
-    body
-    |> Enum.sort_by(&Map.get(&1, "id"))
-    |> Enum.map(&json_rpc_result/1)
+  defp response_content(%{body: body}, requests) when is_list(body) do
+    responses = body |> Enum.sort_by(& &1["id"]) |> Enum.map(&extract_result/1)
+
+    requests
+    |> Enum.sort_by(& &1.id)
+    |> Enum.map(& &1.method)
+    |> Enum.zip(responses)
+    |> Enum.map(&decode_result/1)
   end
 
-  defp response_content(%{body: body}), do: json_rpc_result(body)
+  defp response_content(%{body: response}, request) do
+    decode_result({request.method, extract_result(response)})
+  end
 
-  defp json_rpc_result(%{"result" => %{"value" => value}}), do: value
-  defp json_rpc_result(%{"result" => result}), do: result
-  defp json_rpc_result(%{"error" => error}), do: error
+  defp extract_result(%{"result" => %{"value" => value}}), do: value
+  defp extract_result(%{"result" => result}), do: result
+  defp extract_result(other), do: other
+
+  defp decode_result({_, %{"error" => error}}), do: {:error, error}
+
+  defp decode_result({"requestAirdrop", airdrop_tx}) do
+    {:ok, B58.decode58!(airdrop_tx)}
+  end
+
+  defp decode_result({"getRecentBlockhash", blockhash_result}) do
+    {:ok, update_in(blockhash_result, ["blockhash"], &B58.decode58!/1)}
+  end
+
+  defp decode_result({"sendTransaction", signature}) do
+    {:ok, B58.decode58!(signature)}
+  end
+
+  defp decode_result({_method, result}), do: {:ok, result}
 end
