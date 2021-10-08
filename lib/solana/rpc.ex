43d3c2fdeb519aee2bb9aeb68c713729c2656819
dcrck <derek@meer.email>
2021-10-08 14:14:32 -0700

    add Solana.RPC.send_and_confirm/4
    add a helper to send and confirm transactions

--- lib/solana/rpc.ex
+++ lib/solana/rpc.ex
@@ -1,5 +1,8 @@
 defmodule Solana.RPC do
   use Supervisor
+  require Logger
+
+  alias Solana.RPC
 
   def start_link(config) do
     Supervisor.start_link(__MODULE__, config, name: __MODULE__)
@@ -11,9 +14,9 @@ defmodule Solana.RPC do
 
   defp children(config) do
     [
-      Solana.RPC.Client,
-      Solana.RPC.RateLimiter,
-      {Solana.RPC.Runner, config}
+      RPC.Client,
+      RPC.RateLimiter,
+      {RPC.Runner, config}
     ]
   end
 
@@ -24,7 +27,7 @@ defmodule Solana.RPC do
   def client(rpc) when is_pid(rpc) do
     rpc
     |> Supervisor.which_children()
-    |> Enum.find(&(elem(&1, 0) == Solana.RPC.Client))
+    |> Enum.find(&(elem(&1, 0) == RPC.Client))
     |> case do
       nil -> nil
       child -> elem(child, 1)
@@ -34,7 +37,7 @@ defmodule Solana.RPC do
   def client(config = %{}) do
     middleware = [
       {Tesla.Middleware.BaseUrl, url(config)},
-      Solana.RPC.Middleware,
+      RPC.Middleware,
       Tesla.Middleware.JSON,
       {Tesla.Middleware.Retry, retry_opts(config)}
     ]
@@ -46,7 +49,47 @@ defmodule Solana.RPC do
   Sends the provided requests to the configured Solana RPC endpoint.
   """
   def send(client, requests) do
-    Solana.RPC.Client.send(client, requests)
+    RPC.Client.send(client, requests)
+  end
+
+  @doc """
+  Sends the provided transactions to the configured RPC endpoint, then confirms them.
+  Returns a tuple containing all the transactions in the order they were confirmed, OR
+  an error tuple containing the list of all the transactions that were confirmed
+  before the error occurred.
+  """
+  @spec send_and_confirm(any, pid, [Solana.Transaction.t()] | Solana.Transaction.t, keyword) :: {:ok, [binary]} | {:error, :timeout, [binary]}
+  def send_and_confirm(client, tracker, txs, opts \\ []) do
+    timeout = Keyword.get(opts, :timeout, 5_000)
+    request_opts = Keyword.take(opts, [:commitment])
+    requests = Enum.map(List.wrap(txs), &RPC.Request.send_transaction(&1, request_opts))
+    signatures =
+      client
+      |> RPC.send(requests)
+      |> Enum.flat_map(fn
+        {:ok, signature} ->
+          [signature]
+        {:error, error} ->
+          Logger.error("error sending transaction: #{inspect error}")
+          []
+      end)
+    :ok = RPC.Tracker.start_tracking(tracker, signatures, request_opts)
+
+    await_confirmations(signatures, timeout, [])
+  end
+
+  defp await_confirmations([], _, confirmed), do: {:ok, confirmed}
+
+  defp await_confirmations(signatures, timeout, done) do
+    receive do
+      {:ok, confirmed} ->
+        MapSet.new(signatures)
+        |> MapSet.difference(MapSet.new(confirmed))
+        |> MapSet.to_list()
+        |> await_confirmations(timeout, List.flatten([done, confirmed]))
+    after
+      timeout -> {:error, :timeout, done}
+    end
   end
 
   defp url(%{network: network}) when network in ["devnet", "mainnet-beta", "testnet"] do
