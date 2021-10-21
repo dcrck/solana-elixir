b193ee618355eba888e54cda07c5828174786ba7
dcrck <derek@meer.email>
2021-10-21 15:58:05 -0400

    add SystemProgram function documentation

--- lib/solana/system_program.ex
+++ lib/solana/system_program.ex
@@ -1,47 +1,63 @@
 defmodule Solana.SystemProgram do
+  @moduledoc """
+  Functions for interacting with Solana's [System
+  Program](https://docs.solana.com/developing/runtime-facilities/programs#system-program)
+  """
+
   alias Solana.{Instruction, Account}
   import Solana.Helpers
 
+  @doc """
+  The System Program's program ID.
+  """
   def id(), do: Solana.pubkey!("11111111111111111111111111111111")
 
-  def create_account(opts) do
-    schema = [
-      lamports: [
-        type: :pos_integer,
-        required: true,
-        doc: "Amount of lamports to transfer to the created account"
-      ],
-      space: [
-        type: :non_neg_integer,
-        required: true,
-        doc: "Amount of space in bytes to allocate to the created account"
-      ],
-      from: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "The account that will transfer lamports to the created account"
-      ],
-      new: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Public key of the created account"
-      ],
-      program_id: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Public key of the program which will own the created account"
-      ],
-      base: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Base public key to use to derive the created account's address"
-      ],
-      seed: [
-        type: :string,
-        doc: "Seed to use to derive the created account's address"
-      ]
+  @create_account_schema [
+    lamports: [
+      type: :pos_integer,
+      required: true,
+      doc: "Amount of lamports to transfer to the created account"
+    ],
+    space: [
+      type: :non_neg_integer,
+      required: true,
+      doc: "Amount of space in bytes to allocate to the created account"
+    ],
+    from: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "The account that will transfer lamports to the created account"
+    ],
+    new: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Public key of the created account"
+    ],
+    program_id: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Public key of the program which will own the created account"
+    ],
+    base: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Base public key to use to derive the created account's address"
+    ],
+    seed: [
+      type: :string,
+      doc: "Seed to use to derive the created account's address"
     ]
+  ]
+  @doc """
+  Generates instructions to create a new account. Accepts a `new` address
+  generated via `Solana.Key.with_seed/3`, as long as the `base` key and `seed`
+  used to generate that address are provided.
 
-    case validate(opts, schema) do
+  ## Options
+
+  #{NimbleOptions.docs(@create_account_schema)}
+  """
+  def create_account(opts) do
+    case validate(opts, @create_account_schema) do
       {:ok, params} ->
         maybe_with_seed(
           params,
@@ -55,38 +71,47 @@ defmodule Solana.SystemProgram do
     end
   end
 
-  def transfer(opts) do
-    schema = [
-      lamports: [
-        type: :pos_integer,
-        required: true,
-        doc: "Amount of lamports to transfer"
-      ],
-      from: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Account that will transfer lamports"
-      ],
-      to: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Account that will receive the transferred lamports"
-      ],
-      base: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Base public key to use to derive the funding account address"
-      ],
-      seed: [
-        type: :string,
-        doc: "Seed to use to derive the funding account address"
-      ],
-      program_id: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Program ID to use to derive the funding account address"
-      ]
+  @transfer_schema [
+    lamports: [
+      type: :pos_integer,
+      required: true,
+      doc: "Amount of lamports to transfer"
+    ],
+    from: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Account that will transfer lamports"
+    ],
+    to: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Account that will receive the transferred lamports"
+    ],
+    base: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Base public key to use to derive the funding account address"
+    ],
+    seed: [
+      type: :string,
+      doc: "Seed to use to derive the funding account address"
+    ],
+    program_id: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Program ID to use to derive the funding account address"
     ]
+  ]
+  @doc """
+  Generates instructions to transfer lamports from one account to another.
+  Accepts a `from` address generated via `Solana.Key.with_seed/3`, as long as the
+  `base` key, `program_id`, and `seed` used to generate that address are
+  provided.
 
-    case validate(opts, schema) do
+  ## Options
+
+  #{NimbleOptions.docs(@transfer_schema)}
+  """
+  def transfer(opts) do
+    case validate(opts, @transfer_schema) do
       {:ok, params} ->
         maybe_with_seed(
           params,
@@ -99,29 +124,37 @@ defmodule Solana.SystemProgram do
     end
   end
 
-  def assign(opts) do
-    schema = [
-      account: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Public key for the account which will receive a new owner"
-      ],
-      program_id: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Program ID to assign as the owner"
-      ],
-      base: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Base public key to use to derive the assigned account address"
-      ],
-      seed: [
-        type: :string,
-        doc: "Seed to use to derive the assigned account address"
-      ]
+  @assign_schema [
+    account: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Public key for the account which will receive a new owner"
+    ],
+    program_id: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Program ID to assign as the owner"
+    ],
+    base: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Base public key to use to derive the assigned account address"
+    ],
+    seed: [
+      type: :string,
+      doc: "Seed to use to derive the assigned account address"
     ]
+  ]
+  @doc """
+  Generates instructions to assign account ownership to a program.
+  Accepts an `account` address generated via `Solana.Key.with_seed/3`, as long
+  as the `base` key and `seed` used to generate that address are provided.
 
-    case validate(opts, schema) do
+  ## Options
+
+  #{NimbleOptions.docs(@assign_schema)}
+  """
+  def assign(opts) do
+    case validate(opts, @assign_schema) do
       {:ok, params} ->
         maybe_with_seed(
           params,
@@ -135,33 +168,42 @@ defmodule Solana.SystemProgram do
     end
   end
 
-  def allocate(opts) do
-    schema = [
-      account: [
-        type: {:custom, Solana.Key, :check, []},
-        required: true,
-        doc: "Public key for the account to allocate"
-      ],
-      space: [
-        type: :non_neg_integer,
-        required: true,
-        doc: "Amount of space in bytes to allocate"
-      ],
-      program_id: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Program ID to assign as the owner of the allocated account"
-      ],
-      base: [
-        type: {:custom, Solana.Key, :check, []},
-        doc: "Base public key to use to derive the allocated account address"
-      ],
-      seed: [
-        type: :string,
-        doc: "Seed to use to derive the allocated account address"
-      ]
+  @allocate_schema [
+    account: [
+      type: {:custom, Solana.Key, :check, []},
+      required: true,
+      doc: "Public key for the account to allocate"
+    ],
+    space: [
+      type: :non_neg_integer,
+      required: true,
+      doc: "Amount of space in bytes to allocate"
+    ],
+    program_id: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Program ID to assign as the owner of the allocated account"
+    ],
+    base: [
+      type: {:custom, Solana.Key, :check, []},
+      doc: "Base public key to use to derive the allocated account address"
+    ],
+    seed: [
+      type: :string,
+      doc: "Seed to use to derive the allocated account address"
     ]
+  ]
+  @doc """
+  Generates instructions to allocate space to an account.
+  Accepts an `account` address generated via `Solana.Key.with_seed/3`, as long
+  as the `base` key, `program_id`, and `seed` used to generate that address are
+  provided.
 
-    case validate(opts, schema) do
+  ## Options
+
+  #{NimbleOptions.docs(@allocate_schema)}
+  """
+  def allocate(opts) do
+    case validate(opts, @allocate_schema) do
       {:ok, params} ->
         maybe_with_seed(
           params,
